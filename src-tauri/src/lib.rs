52f54f71edeb6edf8d6e84fd0e811713b1ef50fe
ibro <sameoldlab@proton.me>
2025-01-06 13:58:56 +0100

    add tauri-oauth-plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,16 @@
+use tauri::{command, Emitter, Window};
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
+#[command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[command]
+fn start_server(window: Window) -> Result<u16, String> {
+    tauri_plugin_oauth::start(move |url| {
+        let _ = window.emit("redirect_uri", url);
+    })
+    .map_err(|err| err.to_string())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -9,9 +18,10 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_oauth::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, start_server])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
