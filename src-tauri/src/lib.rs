bb163b1ebf4f9dbce4e9a32cb8398b6934fad05d
ibro <sameoldlab@proton.me>
2025-01-12 14:06:52 +0100

    start and stop server commands

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,4 @@
-use tauri::{command, Emitter, Window};
+use tauri::{command, AppHandle, Emitter, Window};
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[command]
 fn greet(name: &str) -> String {
@@ -6,10 +6,18 @@ fn greet(name: &str) -> String {
 }
 
 #[command]
-fn start_server(window: Window) -> Result<u16, String> {
-    tauri_plugin_oauth::start(move |url| {
-        let _ = window.emit("redirect_uri", url);
+fn start_server(app: AppHandle) -> Result<u16, String> {
+    tauri_plugin_oauth::start( move |url| {
+        println!("url: {}", url);
+        let _ = app.emit("redirect_uri", url).unwrap();
     })
+    .map_err(|err| err.to_string())
+}
+
+#[command]
+fn stop_server(port: u16 ) -> Result<(), String> {
+    println!("{}", port);
+    tauri_plugin_oauth::cancel(port)
     .map_err(|err| err.to_string())
 }
 
@@ -21,7 +29,7 @@ pub fn run() {
         .plugin(tauri_plugin_oauth::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet, start_server])
+        .invoke_handler(tauri::generate_handler![greet, start_server, stop_server])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
